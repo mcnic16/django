ba6c20d3938742ec62e564b1358436f22b41b1e0
mcnic16 <jamesmcnic@yahoo.co.uk>
2022-09-08 16:47:15 +0000

    templates

--- to_do/views.py
+++ to_do/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def say_hello(request):
+    return HttpResponse("><h1>helolo</h1><p>this is a paragraph</p>")
